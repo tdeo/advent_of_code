0c0c475769e515f7a9fa0f8353f51b9b6ebea30b
Thierry Deo <thierry.deo@gmail.com>
2017-12-15 20:45:10 +0100

    Solve problem 2017 - 11

---
+++ 2017/lib/11_hex.rb
@@ -0,0 +1,48 @@
+class Hex
+  def initialize(input)
+    @current = [0, 0]
+    @moves = input.split(',')
+  end
+
+  def apply!(move)
+    case move
+    when 'n'
+      @current[1] += 2
+    when 'ne'
+      @current[0] += 1
+      @current[1] += 1
+    when 'se'
+      @current[0] += 1
+      @current[1] -= 1
+    when 's'
+      @current[1] -= 2
+    when 'sw'
+      @current[0] -= 1
+      @current[1] -= 1
+    when 'nw'
+      @current[0] -= 1
+      @current[1] += 1
+    end
+  end
+
+  def distance_to_origin
+    distance = 0
+    x, y = @current.map(&:abs)
+    min = [x, y].min
+    distance = min + (x - min) / 2 + (y - min) / 2
+  end
+
+  def part1
+    @moves.each { |m| apply!(m) }
+    distance_to_origin
+  end
+
+  def part2
+    max = 0
+    @moves.each do |m|
+      apply!(m)
+      max = [max, distance_to_origin].max
+    end
+    max
+  end
+end
---
+++ 2017/test/11.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative('../lib/11_hex.rb')
+
+describe Hex do
+  before { @k = Hex }
+
+  def test_part1_1
+    assert_equal 3, @k.new('ne,ne,ne').part1
+  end
+
+  def test_part1_2
+    assert_equal 0, @k.new('ne,ne,sw,sw').part1
+  end
+
+  def test_part1_3
+    assert_equal 2, @k.new('ne,ne,s,s').part1
+  end
+
+  def test_part1_4
+    assert_equal 3, @k.new('se,sw,se,sw,sw').part1
+  end
+end
