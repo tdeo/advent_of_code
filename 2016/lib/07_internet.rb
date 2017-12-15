864e4ab8ad79192696d15aa1b57949ef03d4658b
Thierry Deo <thierry.deo@gmail.com>
2017-12-15 20:44:08 +0100

    Solve problem 2016 - 07

---
+++ 2016/lib/07_internet.rb
@@ -0,0 +1,32 @@
+class Internet
+  def initialize(input)
+    @ips = input.strip.split("\n").map(&:strip)
+  end
+
+  def tls?(ip)
+    !(ip =~ /\[[a-z]*([^\[\]])((?!\1).)\2\1/) &&
+      ip =~ /([^\[\]])((?!\1).)\2\1/
+  end
+
+  def part1
+    @ips.count { |ip| tls?(ip) }
+  end
+
+  def ssl?(ip)
+    outside = ip.gsub(/(\[)[^\]]*(\])/, '\1\2')
+    inside = ip.gsub(/(^|\])[^\[]+(\[|$)/, '\1\2')
+
+    outside.chars.each_cons(3) do |a, b, c|
+      next if a == b
+      next unless a == c
+      next if a == '[' || b == '[' || a == ']' || b == ']'
+      next unless inside.include?(b + a + b)
+      return true
+    end
+    false
+  end
+
+  def part2
+    @ips.count { |ip| ssl?(ip) }
+  end
+end
