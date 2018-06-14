a2f68f76cb024b97f4ba1f3a519d3e7ee96f604f
Thierry Deo <thierry.deo@gmail.com>
2018-06-14 16:30:31 +0200

    Solve problem 2016 - 19

---
+++ 2016/lib/19_an_elephant_named_joseph.rb
@@ -0,0 +1,74 @@
+class AnElephantNamedJoseph
+  def initialize(input)
+    @elves = (1..input.strip.to_i).to_a
+  end
+
+  def part1
+    i = 0
+    @elves = init_node(@elves)
+    while @elves.size > 1
+      to_delete = (i + 1) % @elves.size
+      i = to_delete % @elves.size
+      @elves.delete_at(to_delete)
+      print "\033[2K\r#{@elves.size}" if @elves.size % 100_000 == 0
+    end
+    print "\033[2K\r"
+    @elves.value
+  end
+
+  def part2
+    i = 0
+    @elves = init_node(@elves)
+    while @elves.size > 1
+      to_delete = (i + @elves.size / 2) % @elves.size
+      i = (i + 1) % @elves.size unless (to_delete % @elves.size) < (i % @elves.size)
+      @elves.delete_at(to_delete).to_s
+      print "\033[2K\r#{@elves.size}" if @elves.size % 100_000 == 0
+    end
+    print "\033[2K\r"
+    @elves.value
+  end
+end
+
+def init_node(array, parent = nil)
+  return nil if array.size == 0
+  n = Node.new(array.first, array.size, parent, [])
+  return n if array.size == 1
+  a, b = init_node(array[1..array.size / 2], n), init_node(array[array.size / 2 + 1..-1], n)
+  n.children = [a, b].compact
+  n
+end
+
+Node = Struct.new(:value, :size, :parent, :children) do
+  def pp(nested = 0)
+    print ' ' * 2 * nested
+    puts "#{value} (#{size} - #{children.size})"
+    children.each { |c| c.pp(nested + 1) }
+  end
+
+  def delete_at(idx)
+    return nil if idx < 0 || idx >= size
+    self.size -= 1
+    if idx == 0
+      to_return = value
+      if children.empty?
+        parent.children.delete_if { |c| c.value == self.value }
+      else
+        child = children.first
+        self.value = child.value
+        self.children = child.children + children[1..-1]
+        child.children.each { |c| c.parent = self }
+      end
+      return to_return
+    else
+      idx -= 1
+      children.each do |c|
+        if idx < c.size
+          return c.delete_at(idx)
+        else
+          idx -= c.size
+        end
+      end
+    end
+  end
+end
---
+++ 2016/test/19.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require_relative('../lib/19_an_elephant_named_joseph.rb')
+
+describe AnElephantNamedJoseph do
+  before { @k = AnElephantNamedJoseph }
+
+  def test_part1
+    assert_equal 3, @k.new('5').part1
+  end
+
+  def test_part1_2
+    assert_equal 5, @k.new('10').part1
+  end
+
+  def test_part1_3
+    assert_equal 5, @k.new('6').part1
+  end
+
+  def test_part1_4
+    assert_equal 1, @k.new('8').part1
+  end
+
+  def test_part2_1
+    assert_equal 2, @k.new('5').part2
+  end
+
+  def test_part2_2
+    assert_equal 6486, @k.new('65535').part2
+  end
+
+  def test_part2_3
+    assert_equal 6487, @k.new('65536').part2
+  end
+
+  def test_part2_4
+    assert_equal 6488, @k.new('65537').part2
+  end
+
+  def test_part2_5
+    assert_equal 27, @k.new('27').part2
+  end
+end
