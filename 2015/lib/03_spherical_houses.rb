1a55132c046891a6e30b379c56ecb0714541c245
Thierry Deo <thierry.deo@gmail.com>
2018-06-18 18:30:03 +0200

    Solve problem 2015 - 03

---
+++ 2015/lib/03_spherical_houses.rb
@@ -0,0 +1,40 @@
+require 'set'
+
+class SphericalHouses
+  def initialize(input)
+    @input = input.strip
+  end
+
+  def part1
+    x = y = 0
+    visited = Set.new
+    visited << [0, 0]
+    @input.each_char do |c|
+      case c
+      when '>' then x += 1
+      when '<' then x -= 1
+      when '^' then y += 1
+      when 'v' then y -= 1
+      end
+      visited << [x, y]
+    end
+    visited.size
+  end
+
+  def part2
+    x = [0, 0]
+    y = [0, 0]
+    visited = Set.new
+    visited << [0, 0]
+    @input.each_char.each_with_index do |c, i|
+      case c
+      when '>' then x[i % 2] += 1
+      when '<' then x[i % 2] -= 1
+      when '^' then y[i % 2] += 1
+      when 'v' then y[i % 2] -= 1
+      end
+      visited << [x[i % 2], y[i % 2]]
+    end
+    visited.size
+  end
+end
---
+++ 2015/test/03.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative('../lib/03_spherical_houses.rb')
+
+describe SphericalHouses do
+  before { @k = SphericalHouses }
+
+  def test_part1
+    assert_equal 2, @k.new('>').part1
+    assert_equal 4, @k.new('^>v<').part1
+    assert_equal 2, @k.new('^v^v^v^v^v').part1
+  end
+
+  def test_part2
+    assert_equal 3, @k.new('^v').part2
+    assert_equal 3, @k.new('^>v<').part2
+    assert_equal 11, @k.new('^v^v^v^v^v').part2
+  end
+end
