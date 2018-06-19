74716a6a74892140c5846a2c215a0692501dc2f9
Thierry Deo <thierry.deo@gmail.com>
2018-06-19 13:35:58 +0200

    Solve problem 2015 - 10

---
+++ 2015/lib/10_elves.rb
@@ -0,0 +1,27 @@
+class Elves
+  def initialize(input)
+    @input = input.strip
+  end
+
+  def say!
+    @input.gsub!('333', 'cc')
+    @input.gsub!('222', 'cb')
+    @input.gsub!('111', 'ca')
+    @input.gsub!('33', 'bc')
+    @input.gsub!('22', 'bb')
+    @input.gsub!('11', 'ba')
+    @input.gsub!('3', 'ac')
+    @input.gsub!('2', 'ab')
+    @input.gsub!('1', 'aa')
+    @input.tr!('abc', '123')
+  end
+
+  def part1(n = 40)
+    n.times { say! }
+    @input.size
+  end
+
+  def part2
+    part1(50)
+  end
+end
---
+++ 2015/test/10.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative('../lib/10_elves.rb')
+
+describe Elves do
+  before { @k = Elves }
+
+  def test_part1
+    assert_equal '11'.size, @k.new('1').part1(1)
+    assert_equal '312211'.size, @k.new('1').part1(5)
+  end
+end
