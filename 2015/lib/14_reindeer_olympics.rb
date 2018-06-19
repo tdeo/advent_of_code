1c7b8c7f55ffdc86b3aa965f7edf5735c2f1aad4
Thierry Deo <thierry.deo@gmail.com>
2018-06-19 16:57:09 +0200

    Solve problem 2015 - 14

---
+++ 2015/lib/14_reindeer_olympics.rb
@@ -0,0 +1,39 @@
+class ReindeerOlympics
+  def initialize(input)
+    @reindeers = {}
+    input.strip.each_line do |l|
+      l =~ %r|^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.$|
+      @reindeers[$1] = { speed: $2.to_i, duration: $3.to_i, rest: $4.to_i, score: 0 }
+    end
+  end
+
+  def distance(deer, duration)
+    cycles = duration / (deer[:duration] + deer[:rest])
+    remainder = duration % (deer[:duration] + deer[:rest])
+    (cycles * deer[:duration]+ [remainder, deer[:duration]].min) * deer[:speed]
+  end
+
+  def part1(duration = 2503)
+    @reindeers.each_value.map do |deer|
+      distance(deer, duration)
+    end.max
+  end
+
+  def part2(duration = 2503)
+    (1..duration).each do |i|
+      winners = []
+      best_dist = 0
+      @reindeers.each_value do |deer|
+        d = distance(deer, i)
+        if d == best_dist
+          winners << deer
+        elsif d > best_dist
+          winners = [deer]
+          best_dist = d
+        end
+      end
+      winners.each { |w| w[:score] += 1 }
+    end
+    @reindeers.each_value.map { |r| r[:score] }.max
+  end
+end
