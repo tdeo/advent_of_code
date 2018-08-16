2ffbb05533dd9a9ac61f2415af42631054913316
Thierry Deo <thierry.deo@gmail.com>
2018-08-16 09:28:56 +0200

    Solve problem 2015 - 22

---
+++ 2015/lib/22_wizard_simulator.rb
@@ -0,0 +1,166 @@
+require 'set'
+
+class WizardSimulator
+  def initialize(input)
+    @input = input
+    @spells = [
+      { name: :magic_missile, mana_cost: 53 },
+      { name: :drain, mana_cost: 73 },
+      { name: :shield, mana_cost: 113 },
+      { name: :poison, mana_cost: 173 },
+      { name: :recharge, mana_cost: 229 },
+    ]
+    @callbacks = []
+    @turn = 0
+    @me = {
+      mana: 500,
+      health: 50,
+      armor: 0,
+    }
+    @history = []
+    @total_mana = 0
+    @boss = {
+      health: input.split("\n").first.split(':').last.strip.to_i,
+      damage: input.split("\n").last.split(':').last.strip.to_i,
+    }
+  end
+
+  def magic_missile
+    @boss[:health] -= 4
+  end
+
+  def drain
+    @boss[:health] -= 2
+    @me[:health] += 2
+  end
+
+  def shield
+    @me[:armor] += 7
+    @callbacks << { turn: @turn + 7, effect: :shield_effect }
+  end
+
+  def shield_effect
+    @me[:armor] -= 7
+  end
+
+  def poison
+    (1..6).each do |i|
+      @callbacks << { turn: @turn + i, effect: :poison_effect }
+    end
+  end
+
+  def poison_effect
+    @boss[:health] -= 3
+  end
+
+  def recharge
+    (1..5).each do |i|
+      @callbacks << { turn: @turn + i, effect: :recharge_effect }
+    end
+  end
+
+  def recharge_effect
+    @me[:mana] += 101
+  end
+
+  def win?
+    @boss[:health] <= 0
+  end
+
+  def boss
+    @me[:health] -= [1, @boss[:damage] - @me[:armor]].max
+  end
+
+  def available?(spell)
+    !(@callbacks.map { |e| e[:effect] }.include?(:"#{spell}_effect"))
+  end
+
+  def next_turn!(hard: false)
+    @turn += 1
+    if @turn % 2 == 0 && hard
+      @me[:health] -= 1
+      @me[:health] -= 1000 if @me[:health] <= 0
+    end
+    @callbacks.each do |c|
+      next unless c[:turn] == @turn
+      __send__(c[:effect])
+    end
+    @callbacks.delete_if { |c| c[:turn] <= @turn }
+  end
+
+  def run_action(name)
+    spell = @spells.find { |s| s[:name] == name.to_sym}
+    @history << name
+    @me[:mana] -= spell[:mana_cost] unless spell.nil?
+    @total_mana += spell[:mana_cost] unless spell.nil?
+    __send__(name.to_sym)
+  end
+
+  def available_actions
+    return [] if @me[:health] <= 0
+    if @turn % 2 == 1
+      [:boss]
+    else
+      @spells.map { |s| s[:name] if available?(s[:name]) && @me[:mana] >= s[:mana_cost] }.compact
+    end
+  end
+
+  def dead?
+    @me[:health] <= 0
+  end
+
+  def total_mana
+    @total_mana
+  end
+
+  def part1
+    self.class.part1(self)
+  end
+
+  def hash
+    [@me, @callbacks, @boss].hash
+  end
+
+  def self.part1(a)
+    queue = [a]
+    viewed = Set.new([a.hash])
+    while !queue.empty?
+      a = queue.shift
+      return a.total_mana if a.win?
+      a.available_actions.each do |act|
+        b = Marshal::load(Marshal.dump(a))
+        b.run_action(act)
+        next if viewed.include?(b.hash)
+        viewed << b.hash
+        b.next_turn!
+        queue.push(b) unless b.dead?
+      end
+      queue.sort_by! { |g| g.total_mana }
+    end
+    nil
+  end
+
+  def part2
+    @me[:health] -= 1 # Hard mode
+    self.class.part2(self)
+  end
+
+  def self.part2(a)
+    queue = [a]
+    viewed = Set.new([a.hash])
+    while !queue.empty?
+      a = queue.shift
+      return a.total_mana if a.win?
+      a.available_actions.each do |act|
+        b = Marshal::load(Marshal.dump(a))
+        b.run_action(act)
+        next if viewed.include?(b.hash)
+        viewed << b.hash
+        b.next_turn!(hard: true)
+        queue.push(b) unless b.dead?
+      end
+      queue.sort_by! { |g| g.total_mana }
+    end
+    nil
+  end
+end
---
+++ 2015/test/22.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require_relative('../lib/22_wizard_simulator.rb')
+
+describe WizardSimulator do
+  before { @k = WizardSimulator }
+
+  def test_part1
+    i = @k.new("Hit Points: 13
+Damage: 8")
+    i.instance_variable_set(:@me, {
+      mana: 250,
+      health: 10,
+      armor: 0,
+    })
+    assert_equal 173 + 53, i.part1
+  end
+end
