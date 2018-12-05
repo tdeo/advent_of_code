b109e582997926d5a1c8b9c2540cbc0d7c036f3b
Thierry Deo <thierry.deo@gmail.com>
2018-12-05 08:50:01 +0100

    Solve problem 2018 - 05

---
+++ 2018/lib/05_alchemical_reaction.rb
@@ -0,0 +1,31 @@
+class AlchemicalReaction
+  def initialize(input)
+    @input = input.strip
+  end
+
+  def part1(delete: {})
+    @rest = ''
+    (0..@input.size - 1).each do |i|
+      if delete.key?(@input[i])
+        # Do nothing, used for part2
+      elsif @rest.empty?
+        @rest << @input[i]
+      elsif (@input[i].ord - @rest[-1].ord).abs == ('a'.ord - 'A'.ord).abs
+        @rest = @rest[0...-1]
+      else
+        @rest << @input[i]
+      end
+    end
+    @rest.size
+  end
+
+  def part2
+    best = part1
+    orig_input = @rest.dup
+    @input.downcase.chars.uniq.each do |c|
+      @input = orig_input.dup
+      best = [best, part1(delete: { c => true, c.upcase => true })].min
+    end
+    best
+  end
+end
---
+++ 2018/test/05.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require_relative('../lib/05_alchemical_reaction.rb')
+
+describe AlchemicalReaction do
+  before { @k = AlchemicalReaction }
+
+  def test_part1_1
+    assert_equal 0, @k.new('aA').part1
+  end
+
+  def test_part1_2
+    assert_equal 0, @k.new('abBA').part1
+  end
+
+  def test_part1_3
+    assert_equal 4, @k.new('abAB').part1
+  end
+
+  def test_part1_4
+    assert_equal 6, @k.new('aabAAB').part1
+  end
+
+  def test_part1_5
+    assert_equal 10, @k.new('dabAcCaCBAcCcaDA').part1
+  end
+
+  def test_part2
+    assert_equal 4, @k.new('dabAcCaCBAcCcaDA').part2
+  end
+end
