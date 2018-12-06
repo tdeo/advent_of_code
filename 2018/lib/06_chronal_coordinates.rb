2f83c6b790d8ac4a5ee5be8768c72766e026482d
Thierry Deo <thierry.deo@gmail.com>
2018-12-06 08:44:53 +0100

    Solve problem 2018 - 06

---
+++ 2018/lib/06_chronal_coordinates.rb
@@ -0,0 +1,69 @@
+class ChronalCoordinates
+  def initialize(input)
+    @input = input
+
+    @points = @input.split("\n").map { |e| e.split(', ').map(&:to_i) }
+  end
+
+  def d(a, b)
+    (a[0] - b[0]).abs + (a[1] - b[1]).abs
+  end
+
+  def closest(x, y)
+    best_d = Float::INFINITY
+    best_point = nil
+    @points.each_with_index do |point, i|
+      dist = d([x, y], point)
+      if dist < best_d
+        best_point = i
+        best_d = dist
+      elsif dist == best_d
+        best_point = nil
+      end
+    end
+    best_point
+  end
+
+  def dist_sum(x, y, threshold)
+    s = 0
+    @points.each_with_index do |point, i|
+      s +=  d([x, y], point)
+      return s if s > threshold
+    end
+    s
+  end
+
+  def part1
+    zones = Hash.new { |h, k| h[k] = 0 }
+
+    (@points.map(&:first).min..@points.map(&:first).max).each do |x|
+      (@points.map(&:last).min..@points.map(&:last).max).each do |y|
+        zones[closest(x, y)] += 1
+      end
+    end
+
+    [@points.map(&:first).min, @points.map(&:first).max].each do |x|
+      (@points.map(&:last).min..@points.map(&:last).max).each do |y|
+        zones.delete(closest(x, y))
+      end
+    end
+
+    (@points.map(&:first).min..@points.map(&:first).max).each do |x|
+      [@points.map(&:last).min, @points.map(&:last).max].each do |y|
+        zones.delete(closest(x, y))
+      end
+    end
+    zones.delete(nil)
+    zones.values.max
+  end
+
+  def part2(n = 10_000)
+    count = 0
+    (@points.map(&:first).min..@points.map(&:first).max).each do |x|
+      (@points.map(&:last).min..@points.map(&:last).max).each do |y|
+        count += 1 if dist_sum(x, y, n) < n
+      end
+    end
+    count
+  end
+end
---
+++ 2018/test/06.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require_relative('../lib/06_chronal_coordinates.rb')
+
+describe ChronalCoordinates do
+  before { @k = ChronalCoordinates }
+
+  def test_part1
+    assert_equal 17, @k.new('1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9').part1
+  end
+
+  def test_part2
+    assert_equal 16, @k.new('1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9').part2(32)
+  end
+end
