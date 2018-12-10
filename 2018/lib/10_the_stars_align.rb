2213dc20aa811b21577c8ad462ece6b5077e3fce
Thierry Deo <thierry.deo@gmail.com>
2018-12-10 07:53:16 +0100

    Solve problem 2018 - 10

---
+++ 2018/lib/10_the_stars_align.rb
@@ -0,0 +1,70 @@
+class TheStarsAlign
+  def initialize(input)
+    @input = input
+    @stars = []
+    @input.each_line do |l|
+      m = l.match(/position=<(\s?-?\d+), (\s?-?\d+)> velocity=<(\s?-?\d+), (\s?-?\d+)>/)
+      @stars << m[1..4].map(&:to_i)
+    end
+  end
+
+  def area
+    w = @stars.map { |e| e[0] }.minmax
+    h = @stars.map { |e| e[1] }.minmax
+    (w[1] - w[0]) * (h[1] - h[0])
+  end
+
+  def move!
+    @stars.each do |s|
+      s[0] += s[2]
+      s[1] += s[3]
+    end
+  end
+
+  def reverse_move!
+    @stars.each do |s|
+      s[0] -= s[2]
+      s[1] -= s[3]
+    end
+  end
+
+  def print!
+    w = @stars.map { |e| e[0] }.minmax
+    h = @stars.map { |e| e[1] }.minmax
+
+    view = (h[0]..h[1]).map do |i|
+      '.' * (w[1] - w[0] + 1)
+    end
+
+    @stars.each do |s|
+      view[s[1] - h[0]][s[0] - w[0]] = '#'
+    end
+
+    puts view.join("\n")
+  end
+
+  def part1
+    a = Float::INFINITY
+    while true
+      move!
+      b = area
+      break if b > a
+      a = b
+    end
+    reverse_move!
+    print!
+  end
+
+  def part2
+    a = Float::INFINITY
+    time = 0
+    while true
+      move!
+      time += 1
+      b = area
+      break if b > a
+      a = b
+    end
+    time - 1
+  end
+end
