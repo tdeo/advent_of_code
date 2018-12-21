4b5d887319ef1a0a2aff48f2ba913a901134b25d
Thierry Deo <thierry.deo@gmail.com>
2018-12-21 11:31:10 +0100

    Solve problem 2018 - 21

---
+++ 2018/lib/21_chronal_conversion.rb
@@ -0,0 +1,64 @@
+require_relative '19_go_with_the_flow'
+
+class ChronalConversion < GoWithTheFlow
+  def initialize(input)
+    super
+  end
+
+  def round!
+    super
+  end
+
+  def part1
+    # The halting condition of the program is r4 == r0 on instruction 28
+    while @ptr >= 0 && @ptr < @instructions.size
+      return @regs[4] if @ptr == 28
+      round!
+    end
+  end
+
+  def n(r4)
+    r1 = r4 | 65536
+
+    r4 = 2024736
+
+    while r1 > 0
+      r4 += (r1 & 255)
+      r4 *= 65899
+      r4 &= 16777215
+      r1 /= 256
+    end
+    r4
+  end
+
+  def part2
+    # it's again all about decompiling:
+    # r4 = 123
+    # r4 = r4 & 456
+    # r4 = (r4 == 72)
+
+    # exit 1 if r4 != 72 # End initialization
+    # r4 = 0
+    # while true
+    #   r1 = r4 | 65536
+    #   r4 = 2024736
+    #   while r1 > 0
+    #     r4 += (r1 & 255)
+    #     r4 *= 65899
+    #     r4 &= 16777215
+    #     r1 /= 256
+    #   end
+    #   exit 0 if r4 == r0
+    # end
+
+    r4 = 7129803
+    prev = r4
+    viewed = { r4 => true }
+    while true
+      r4 = n(r4)
+      return prev if viewed[r4]
+      prev = r4
+      viewed[r4] = true
+    end
+  end
+end
