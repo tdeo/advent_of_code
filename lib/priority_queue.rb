dc0cb7c3d5b5a3fd7e0f866d30208b33fb9c0747
Thierry Deo <thierry.deo@gmail.com>
2018-12-22 12:46:28 +0100

    Add priority queue lib

---
+++ lib/priority_queue.rb
@@ -0,0 +1,58 @@
+class PriorityQueue
+  def initialize(&block)
+    @q = [nil]
+    @block = block
+    fail 'Unrecognized block arity' if block && (block.arity < 1 || block.arity > 2)
+  end
+
+  def <<(el)
+    @q << el
+    bubble_up(@q.size - 1)
+  end
+
+  def pop
+    exchange(1, @q.size - 1)
+    el = @q.pop
+    bubble_down(1)
+    el
+  end
+
+  def empty?
+    @q.size <= 1
+  end
+
+  private
+
+  def lte(a, b)
+    if @block.nil?
+      (a <=> b) <= 0
+    elsif @block.arity == 1
+      @block.call(a) <= @block.call(b)
+    else
+      @block.call(a, b) <= 0
+    end
+  end
+
+  def exchange(a, b)
+    @q[a], @q[b] = @q[b], @q[a]
+  end
+
+  def bubble_up(i)
+    parent = i / 2
+    return if i <= 1
+    return if lte(@q[parent], @q[i])
+    exchange(i, parent)
+    bubble_up(parent)
+  end
+
+  def bubble_down(i)
+    child = 2*i
+    return if child >= @q.size
+    lchild = @q[child]
+    rchild = @q[child + 1]
+    child += 1 if child < @q.size - 1 && lte(rchild, lchild)
+    return if lte(@q[i], @q[child])
+    exchange(i, child)
+    bubble_down(child)
+  end
+end
---
+++ test/priority_queue_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require_relative '../lib/priority_queue.rb'
+
+describe PriorityQueue do
+  before { @k = PriorityQueue }
+
+  def test_no_block
+    q = @k.new
+    q << 7
+    q << 3
+    q << 5
+    q << 1
+    q << 8
+    assert_equal 1, q.pop
+    assert_equal 3, q.pop
+    assert_equal 5, q.pop
+    assert_equal 7, q.pop
+    assert_equal 8, q.pop
+  end
+
+  def test_block_arity_1
+    q = @k.new { |e| e.last }
+    q << ['a', 7]
+    q << ['b', 3]
+    q << ['c', 5]
+    q << ['d', 1]
+    q << ['e', 8]
+    assert_equal ['d', 1], q.pop
+    assert_equal ['b', 3], q.pop
+    assert_equal ['c', 5], q.pop
+    assert_equal ['a', 7], q.pop
+    assert_equal ['e', 8], q.pop
+  end
+
+  def test_block_arity_2
+    q = @k.new { |a, b| a.last - b.last }
+    q << ['a', 7]
+    q << ['b', 3]
+    q << ['c', 5]
+    q << ['d', 1]
+    q << ['e', 8]
+    assert_equal ['d', 1], q.pop
+    assert_equal ['b', 3], q.pop
+    assert_equal ['c', 5], q.pop
+    assert_equal ['a', 7], q.pop
+    assert_equal ['e', 8], q.pop
+  end
+end
