02776b11db164f83c621be362ab916cf5bdd3400
Thierry Deo <thierry.deo@gmail.com>
2019-04-06 10:43:44 +0200

    Random first start for 2018-23

--- 2018/lib/23_experimental_emergency_teleportation.rb
+++ 2018/lib/23_experimental_emergency_teleportation.rb
@@ -27,7 +27,6 @@ class ExperimentalEmergencyTeleportation
     end
     c
   end
-
   def connected?(i1, i2)
     return true if i1 == i2
     @connected ||= Hash.new { |h, k| h[k] = {} }
@@ -37,7 +36,9 @@ class ExperimentalEmergencyTeleportation
   def strongly_connected(idxs)
     return [[]] if idxs.empty?
 
-    a = idxs.sample
+    idxs.shuffle!
+
+    a = idxs.first
     c = []
     nc = []
     idxs[1..-1].each do |i|
@@ -56,7 +57,63 @@ class ExperimentalEmergencyTeleportation
     res
   end
 
+  def intersected_bots(point)
+    @bots.select { |b| dist(b, point) <= b[-1] }
+  end
+
+  def intersect_count(point)
+    intersected_bots(point).size
+  end
+
+
   def part2
+    part2a
+  end
+
+  def center_of_mass(bots)
+    if @rmin.nil? || @rmax.nil?
+      @rmin, @rmax = @bots.map(&:last).minmax
+    end
+    # puts @rmax, @rmin
+    x = y = z = 0
+    r = 0.0
+    bots.each do |b|
+      w = (2 * @rmax - b[3]) / (@rmax - @rmin)
+      x += b[0] * w
+      y += b[1] * w
+      z += b[2] * w
+      r += w
+    end
+    [x / r, y / r, z / r].map(&:round)
+  end
+
+  def part2c
+    c = [0, 0, 0]
+    bots = @bots
+    12.times do
+      c = center_of_mass(bots)
+      puts intersect_count(c)
+      bots = intersected_bots(c)
+    end
+  end
+
+  def part2b
+    xmin, xmax = @bots.map { |b| b[0] }.minmax
+    ymin, ymax = @bots.map { |b| b[1] }.minmax
+    zmin, zmax = @bots.map { |b| b[2] }.minmax
+    step = @bots.map(&:last).min
+    step /= 6
+
+    (xmin..xmax).step(step).each do |x|
+      (ymin..ymax).step(step).each do |y|
+        (zmin..zmax).step(step).each do |z|
+          puts "#{x} #{y} #{z} #{intersect_count([x, y, z])}" if intersect_count(x, y, z) > 700
+        end
+      end
+    end
+  end
+
+  def part2a
     (0...@bots.size).each do |i|
       (0...@bots.size).each do |j|
         connected?(i, j)
@@ -64,6 +121,7 @@ class ExperimentalEmergencyTeleportation
     end
 
     comp = strongly_connected (0...@bots.size).to_a
+    puts "Biggest component size: #{comp[0].size}"
     fail if comp.size != 1 # Edge case not handled
 
     r = 0
