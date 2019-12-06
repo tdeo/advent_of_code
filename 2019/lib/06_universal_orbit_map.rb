6be39a611229b8e5d13f23a588911a9615fc7d06
Thierry Deo <thierry@reecall.co>
2019-12-06 13:24:28 +0100

    Solve problem 2019 - 06

---
+++ 2019/lib/06_universal_orbit_map.rb
@@ -0,0 +1,45 @@
+class UniversalOrbitMap
+  def initialize(input)
+    @input = input
+    @orbits = Hash.new { |h, k| h[k] = [] }
+    @rorbits = {}
+    input.each_line do |l|
+      a, b = l.strip.split(')')
+      @orbits[a] << b
+      @rorbits[b] = a
+    end
+  end
+
+  def children(node)
+    r = 0
+    @orbits[node].each do |c|
+      r += children(c) + 1
+    end
+    r
+  end
+
+  def total(node)
+    r = 0
+    @orbits[node].map do |c|
+      r += total(c) + children(c) + 1
+    end
+    r
+  end
+
+  def part1
+    total('COM')
+  end
+
+  def parents(node)
+    res = [node]
+    res << @rorbits[res[-1]] while @rorbits[res[-1]]
+    res
+  end
+
+  def part2
+    a = parents('YOU')
+    b = parents('SAN')
+    c = a & b
+    ((a-c).size + (b-c).size) - 2
+  end
+end
---
+++ 2019/test/06.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require_relative('../lib/06_universal_orbit_map.rb')
+
+describe UniversalOrbitMap do
+  before { @k = UniversalOrbitMap }
+
+  def test_part1
+    assert_equal 42, @k.new('COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+').part1
+  end
+
+  def test_part2
+    assert_equal 4, @k.new('COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN').part2
+  end
+end
