08178f8a04efecb75d4799ed8c701463caac3167
Thierry Deo <thierry.deo@gmail.com>
2019-12-15 12:26:42 +0100

    Solve problem 2019 - 14

---
+++ 2019/lib/14_space_stoichiometry.rb
@@ -0,0 +1,51 @@
+class SpaceStoichiometry
+  def initialize(input)
+    @input = input
+    @reactions = {}
+    @input.each_line do |l|
+      left, right = l.split(' => ')
+      q, val = right.strip.split(' ')
+      @reactions[val] = { q: q.to_i, in: [] }
+      left.strip.split(', ').each do |elem|
+        q2, val2 = elem.split(' ')
+        @reactions[val][:in] << [val2, q2.to_i]
+      end
+    end
+  end
+
+  def part1(target = 1)
+    needs = Hash.new { |h, k| h[k] = 0 }
+    needs['FUEL'] = target
+
+    while true do
+      k, v = needs.find { |kk, vv| kk != 'ORE' && vv > 0 }
+      return needs['ORE'] if k.nil?
+
+      reaction = @reactions[k]
+
+      times = (v * 1.0 / reaction[:q]).ceil
+
+      needs[k] -= times * reaction[:q]
+      reaction[:in].each do |elem|
+        needs[elem[0]] += elem[1] * times
+      end
+    end
+  end
+
+  def part2
+    trillion = 10**12
+    can = 0
+    cannot = trillion + 1
+
+    while cannot > can + 1 do
+      middle = (cannot + can) / 2
+      v = part1(middle)
+      if v > trillion
+        cannot = middle
+      else
+        can = middle
+      end
+    end
+    can
+  end
+end
