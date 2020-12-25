fa85a8c8019d98dc5432acac85bd110c965099ed
Thierry Deo <thierry.deo@gmail.com>
2020-12-25 11:05:18 +0100

    Solve problem 2020 - 09

---
+++ 2020/lib/09_encoding_error.rb
@@ -0,0 +1,59 @@
+class EncodingError
+  def initialize(input)
+    @input = input
+    @numbers = @input.each_line.map(&:to_i)
+  end
+
+  def setup
+    @visible = Hash.new(0)
+    (0...@n).each do |i|
+      @visible[@numbers[i]] += 1
+    end
+  end
+
+  def valid?
+    target = @numbers[@index]
+    (@index - @n...@index).each do |i|
+      return true if @visible.key?(target - @numbers[i])
+    end
+    false
+  end
+
+  def succ!
+    prev = @numbers[@index - @n]
+    if @visible[prev] == 1
+      @visible.delete(prev)
+    else
+      @visible[prev] -= 1
+    end
+    @index += 1
+    @visible[@numbers[@index]] += 1
+  end
+
+  def part1(preamble_size = 25)
+    @n = preamble_size
+    setup
+    @index = preamble_size
+
+    succ! while valid?
+
+    @numbers[@index]
+  end
+
+  def part2(preamble_size = 25)
+    target = part1(preamble_size)
+
+    cumulated = 0
+    viewed = { 0 => 0 }
+    @numbers.each_with_index do |number, i|
+      cumulated += number
+      viewed[cumulated] = i + 1
+
+      if viewed.key?(cumulated - target)
+        start = viewed[cumulated - target]
+
+        return @numbers[start..i].minmax.sum
+      end
+    end
+  end
+end
---
+++ 2020/test/09.rb
@@ -0,0 +1,52 @@
+require 'minitest/autorun'
+require_relative('../lib/09_encoding_error.rb')
+
+describe EncodingError do
+  before { @k = EncodingError }
+
+  def test_part1
+    assert_equal 127, @k.new('35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576').part1(5)
+  end
+
+  def test_part2
+    assert_equal 62, @k.new('35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576').part2(5)
+  end
+end
