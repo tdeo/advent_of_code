6c0d3072ff4770149df4b006b4d1792945e62797
Thierry Deo <thierry.deo@gmail.com>
2020-12-30 14:50:30 +0100

    Solve problem 2020 - 16

---
+++ 2020/lib/16_ticket_translation.rb
@@ -0,0 +1,84 @@
+class TicketTranslation
+  def initialize(input)
+    @input = input
+
+    rules, my_tickets, nearby_tickets = input.split("\n\n")
+
+    @rules = Hash.new { |hash, key| hash[key] = [] }
+    rules.each_line do |line|
+      key, values = line.split(': ')
+      values = values.split(' or ')
+      values.each do |range|
+        @rules[key] << range.split('-').map(&:to_i)
+      end
+    end
+
+    @mine = my_tickets.split("\n")[1].split(',').map(&:to_i)
+    @nearby = nearby_tickets.split("\n")[1..-1].map { |line| line.split(',').map(&:to_i) }
+  end
+
+  def valid?(value, ranges)
+    ranges.any? { |low, high| low <= value && value <= high }
+  end
+
+  def valid_ticket?(ticket)
+    ticket.all? do |value|
+      @rules.values.any? do |ranges|
+        valid?(value, ranges)
+      end
+    end
+  end
+
+  def part1
+    invalid = 0
+
+    @nearby.each do |ticket|
+      ticket.each do |value|
+        next if valid?(value, @rules.values.flatten(1))
+
+        invalid += value
+      end
+    end
+
+    invalid
+  end
+
+  def part2(regex = /^departure/)
+    options = @rules.keys.map { |key| [key, (0...@rules.size).to_a] }.to_h
+
+    @nearby.each do |ticket|
+      next unless valid_ticket?(ticket)
+
+      ticket.each_with_index do |value, i|
+        @rules.each do |key, ranges|
+          next if valid?(value, ranges)
+
+          options[key].delete(i)
+        end
+      end
+    end
+
+    propagated = {}
+    options.size.times do
+      options.each do |key, values|
+        next unless values.size == 1
+        next if propagated.key?(key)
+        propagated[key] = true
+
+        options.each_key do |k2|
+          options[k2].delete(values[0]) if k2 != key
+        end
+      end
+    end
+
+    result = 1
+    options.each do |key, values|
+      next unless key =~ regex
+      fail if values.size > 1
+
+      result *= @mine[values[0]]
+    end
+
+    result
+  end
+end
---
+++ 2020/test/16.rb
@@ -0,0 +1,49 @@
+require 'minitest/autorun'
+require_relative('../lib/16_ticket_translation.rb')
+
+describe TicketTranslation do
+  before { @k = TicketTranslation }
+
+  def test_part1
+    assert_equal 71, @k.new('class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12').part1
+  end
+
+  def test_part2
+    assert_equal 12 * 11, @k.new('class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9').part2(/(class|row)/)
+  end
+
+  def test_part2_2
+    assert_equal 12 * 13, @k.new('class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9').part2(/(class|seat)/)
+  end
+end
