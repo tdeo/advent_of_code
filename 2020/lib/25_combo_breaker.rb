1d370effc30d725a6272dc1fd081a1c719173a05
Thierry Deo <thierry.deo@gmail.com>
2021-01-03 14:44:46 +0100

    Solve problem 2020 - 25

---
+++ 2020/lib/25_combo_breaker.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class ComboBreaker
+  MOD = 20_201_227
+  SUBJECT = 7
+
+  def initialize(input)
+    @input = input
+    @door, @card = input.split("\n").map(&:to_i)
+  end
+
+  def res(value, pow)
+    r = 1
+    pow.times do
+      r = (r * value) % MOD
+    end
+    r
+  end
+
+  def part1
+    powers = {}
+
+    exp = 0
+    val = 1
+
+    loop do
+      break if powers.key?(@door) && powers.key?(@card)
+
+      powers[val] = exp
+      val = (val * SUBJECT) % MOD
+      exp += 1
+    end
+
+    door = res(@door, powers[@card])
+    card = res(@card, powers[@door])
+    raise if door != card
+
+    door
+  end
+
+  def part2
+    true
+  end
+end
---
+++ 2020/test/25.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative('../lib/25_combo_breaker')
+
+describe ComboBreaker do
+  before { @k = ComboBreaker }
+
+  def test_part1
+    assert_equal 14_897_079, @k.new(<<~INPUT).part1
+      5764801
+      17807724
+    INPUT
+  end
+
+  def test_part2
+    assert_equal true, @k.new(<<~INPUT).part2
+    INPUT
+  end
+end
