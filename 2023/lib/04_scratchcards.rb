d74bdcb40db9fd3ce2fa9b7872c8f38cf68ff2b7
Thierry Deo <thierry.deo@gmail.com>
2023-12-09 09:55:27 +0100

    Solve problem 2023 - 04

---
+++ 2023/lib/04_scratchcards.rb
@@ -0,0 +1,43 @@
+# typed: strong
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+
+class Scratchcards
+  extend T::Sig
+
+  sig { params(input: String).void }
+  def initialize(input)
+    @input = input
+    @cards = T.let(
+      @input.split("\n").map do |line|
+        _, numbers = line.split(': ')
+
+        T.must(numbers).split(' | ').map { _1.split.map(&:to_i) }
+      end,
+      T::Array[T::Array[T::Array[Integer]]],
+    )
+  end
+
+  sig { returns(Integer) }
+  def part1
+    @cards.sum do |card|
+      overlap = (T.must(card.first) & T.must(card.last)).size
+      overlap == 0 ? 0 : (2**(overlap - 1)).to_i
+    end
+  end
+
+  sig { returns(Integer) }
+  def part2
+    counts = @cards.map { 1 }
+
+    @cards.each_with_index do |card, i|
+      win_count = (T.must(card.first) & T.must(card.last)).size
+      (i + 1..i + win_count).each do |j|
+        counts[j] = T.must(counts[j]) + T.must(counts[i])
+      end
+    end
+
+    counts.sum
+  end
+end
---
+++ 2023/test/04.rb
@@ -0,0 +1,32 @@
+# typed: strict
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+require 'minitest/autorun'
+require_relative('../lib/04_scratchcards')
+
+class ScratchcardsTest < Minitest::Spec
+  extend T::Sig
+  sig { returns(T.class_of(Scratchcards)) }
+  def described_class = Scratchcards
+
+  sig { returns(String) }
+  def input = <<~INPUT
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+  INPUT
+
+  sig { void }
+  def test_part1
+    assert_equal 13, described_class.new(input).part1
+  end
+
+  sig { void }
+  def test_part2
+    assert_equal 30, described_class.new(input).part2
+  end
+end
