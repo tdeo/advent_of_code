67f490ceb3d5d04359ec63b6ff1e60f3137cf9b0
Thierry Deo <thierry.deo@gmail.com>
2024-12-07 08:10:20 +0100

    Solve problem 2023 - 12

---
+++ 2023/lib/12_hot_springs.rb
@@ -0,0 +1,52 @@
+# typed: strong
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+
+class HotSprings
+  extend T::Sig
+
+  sig { params(input: String).void }
+  def initialize(input)
+    @input = input
+    @problems = T.let(@input.each_line(chomp: true).map do |line|
+      pattern, count = line.split
+
+      [T.must(pattern), T.must(count).split(',').map(&:to_i)]
+    end, T::Array[[String, T::Array[Integer]]],)
+  end
+
+  sig { params(pattern: String, target: T::Array[Integer]).returns(Integer) }
+  def options(pattern, target)
+    if target.empty?
+      return 0 if pattern.include?('#')
+
+      return 1
+    end
+
+    return 0 if pattern.size < target.sum + target.size - 1
+
+    r = 0
+    if !T.must(pattern[-T.must(target.last)..]).include?('.') && (pattern[-T.must(target.last) - 1] || '.') != '#'
+      r += options(T.must(pattern[...-T.must(target.last) - 1]), T.must(target[...-1]))
+    end
+    r += options(T.must(pattern[...-1]), target) unless pattern[-1] == '#'
+
+    r
+  end
+
+  sig { returns(Integer) }
+  def part1
+    @problems.sum do |pattern, target|
+      options(pattern, target)
+    end
+  end
+
+  sig { returns(Integer) }
+  def part2
+    @problems.sum do |pattern, target|
+      print '.'
+      options(([pattern] * 5).join('?'), target * 5)
+    end
+  end
+end
---
+++ 2023/test/12_test.rb
@@ -0,0 +1,32 @@
+# typed: strict
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+require 'minitest/autorun'
+require_relative('../lib/12_hot_springs')
+
+class HotSpringsTest < Minitest::Test
+  extend T::Sig
+  sig { returns(T.class_of(HotSprings)) }
+  def described_class = HotSprings
+
+  sig { returns(String) }
+  def input = <<~INPUT
+    ???.### 1,1,3
+    .??..??...?##. 1,1,3
+    ?#?#?#?#?#?#?#? 1,3,1,6
+    ????.#...#... 4,1,1
+    ????.######..#####. 1,6,5
+    ?###???????? 3,2,1
+  INPUT
+
+  sig { void }
+  def test_part1
+    assert_equal 21, described_class.new(input).part1
+  end
+
+  sig { void }
+  def test_part2
+    assert_equal 525_152, described_class.new(input).part2
+  end
+end
