5ca72a912c7c53fe5791e34f3b1de1d32eea9333
Thierry Deo <thierry@pennylane.com>
2026-08-27 22:26:36 +0200

    Solve problem 2023 - 21 part 2

--- 2023/lib/21_step_counter.rb
+++ 2023/lib/21_step_counter.rb
@@ -55,14 +55,40 @@ class StepCounter < Map
 
   sig { params(steps: Integer).returns(Integer) }
   def part2(steps = 26_501_365)
-    i, j = @start.coords
-    (0...@width).each { raise if at(_1, j)&.value == CellValue::Rock }
-    (0...@height).each { raise if at(i, _1)&.value == CellValue::Rock }
+    raise 'grid is not square' unless @height == @width
+    raise 'uncentered start' unless @start.coords[0] == @start.coords[1]
+    raise 'uncentered start' unless (@start.coords[0] * 2) + 1 == @height
 
-    distances_from(@start)
-    steps2 = steps % (@width + @height)
-    part1(steps2, @start)
+    a = steps % @width
+    final_i = steps / @width
 
-    0
+    first_values = (0..2).map do |i|
+      steps = (i * @width) + a
+      k = (2 * i) + 1
+      new_input = @input.lines(chomp: true).map do |line|
+        line.tr('S', '.') * k
+      end
+      new_input = k.times.flat_map { new_input.map(&:dup) }
+      T.must(new_input[new_input.size / 2])[new_input.size / 2] = 'S'
+      solver = self.class.new(new_input.join("\n"))
+      res = solver.part1(steps)
+      res
+    end
+
+    v0 = first_values[0].to_i
+    v1 = first_values[1].to_i
+    v2 = first_values[2].to_i
+    # the equation should be polynomial: val = a*i^2 + b*i + c
+    # with i = 0, 1, 2, we have:
+    # v0 = c
+    # v1 = a + b + c
+    # v2 = 4a + 2b + c
+    # easily enough:
+    c = v0
+    # v2 - 2 * v1 = 2a - c, so:
+    a = (v2 - (2 * v1) + c) / 2
+    b = v1 - a - c
+
+    (a * final_i * final_i) + (b * final_i) + c
   end
 end
